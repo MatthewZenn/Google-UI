732a1f7a7aa8399d07586d05f8aa91b7e3364db6
Matthew Zenn <spyguy57@gmail.com>
2023-05-20 21:29:45 -0400

    added stuff

--- googleui/www/js/index.js
+++ googleui/www/js/index.js
@@ -1,6 +1,18 @@
-var colors = ["rgb(0, 112, 255)", "rgb(255, 185, 0)", "rgb(238, 52, 52)", "rgb(0, 172, 71)"]
+var colors = ["rgb(0, 112, 255)", "rgb(255, 185, 0)", "rgb(238, 52, 52)", "rgb(0, 172, 71)"];
+var x = 0;
+const menu = document.getElementById("menu");
+const hamburger = document.getElementById("hamburger");
+const overlay = document.getElementById("overlay");
 
-document.getElementById("button1").addEventListener("click", () => {
-    var color = colors[Math.floor(Math.random()*colors.length)]
-    document.getElementById("button1").style.backgroundColor = color;
+hamburger.addEventListener("click", () => {
+    if (x == 0) {
+        x = 1;
+        menu.style.left = "0px";
+        overlay.style.display = "block";
+    }
+    else {
+        x = 0;
+        menu.style.left = "-300px";
+        overlay.style.display = "none";
+    }
 });
